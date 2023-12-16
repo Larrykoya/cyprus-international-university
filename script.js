479a82aa9f62e9d9dce1322057e37402dcf84552
larrykoya <larrykoya@yahoo.com>
2023-12-16 20:07:14 +0100

    index page wrap up

---
+++ script.js
@@ -0,0 +1,6 @@
+let showDropDown = () => {
+  document.getElementById("drop-down").style.visibility = "visible";
+};
+let hideDropDown = () => {
+  document.getElementById("drop-down").style.visibility = "hidden";
+};
