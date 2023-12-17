6cc01cc8328e8f12bbefbbafa8cfbaa7417b53f9
larrykoya <larrykoya@yahoo.com>
2023-12-17 12:17:51 +0100

    index page wrap up

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 let showDropDown = () => {
   document.getElementById("drop-down").style.visibility = "visible";
   document.getElementById("arrow-flip").style.transform = "rotate(180deg)";
+  document.getElementById("student-link").style.color = "aqua";
+  document.getElementById("student-link").style.textDecoration = "underline";
 };
 let hideDropDown = () => {
   document.getElementById("drop-down").style.visibility = "hidden";
   document.getElementById("arrow-flip").style.transform = "rotate(0deg)";
+  document.getElementById("student-link").style.color = "#fff";
+  document.getElementById("student-link").style.textDecoration = "none";
 };
